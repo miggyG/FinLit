ef044a0744d2196b10ca43bd52ca0523c7d940f6
miggyG <54223177+miggyG@users.noreply.github.com>
2022-09-28 14:19:29 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
 # main
+import numpy as np
+import pandas as pd
+from caltick import maincal
+from gettingnews import mainsia
+import yfinance
+# tickerslist = maincal(1)
+
+
+def stockanalysis(tickers, returnformat):
+    print("got tickers")
+    tickerdic = mainsia(tickers, returnformat)
+    print(tickerdic)
+
+
+def calendaryahoo(rtype):
+    summary = (maincal(rtype))
+    print(summary)
+    return summary
+
+
+if __name__ == "__main__":
+    t = calendaryahoo(1)
+    stockanalysis(t, 1)
